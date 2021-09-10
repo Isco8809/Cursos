97524d48b9ac986c1dfc831ea74e9378229345d8
Isco8809 <francisco.echeverri@bambooanalytics.com.co>
2021-09-10 17:17:50 -0500

    Codigo funcional

--- Python/ClaseProyectoFinalPython.py
+++ Python/ClaseProyectoFinalPython.py
@@ -1,8 +1,8 @@
 import json
 #Se crea la clase que se encarga de consultar los datos del usuario (codigo, criptmonedas y cantidad)
 class usuario():
-    def __init__(self):
-        pass
+    def __init__(self, codigo):
+        self.codigo = codigo
 
     def consultarBD(self):
         with open('Python\BD_Usuarios.json') as js:
@@ -10,8 +10,14 @@ class usuario():
             js.close()
         return consulta
 
-    def condigoUsuario(self):
-        consulta = self.consultarBD()
-        for a in consulta['Usuarios'][0]['Criptomonedas'][0]['Cripto']:
+    def codigoUsuario(self):
+        conexion = self.consultarBD()
+        
+        if self.codigo == conexion['Usuarios']['Criptomonedas']:
+            
+        for a in otra['Usuarios'][0]['Criptomonedas'][0]['Cripto']:
             print(a)
         return a
+    
+    # def datosUsuario(self):
+        
--- Python/mainPrincipal.py
+++ Python/mainPrincipal.py
@@ -1,10 +1,14 @@
 import json
-from ClaseProyectoFinalPython import usuario
+#from ClaseProyectoFinalPython import usuario
 
-# with open('Python\BD_Usuarios.json') as js:
-#     consulta = json.load(js)
-#     js.close()
-#     print(consulta)
+with open('Python\BD_Usuarios.json') as js:
+    consulta = json.load(js)
+    js.close()
+    print(consulta)
+    
+name = input()
 
-variable = usuario.condigoUsuario()
-print(variable)
+for a in consulta['Usuarios']:
+    if name in str(a['Codigo']):
+        print(a['Nombre'])
+        pass
