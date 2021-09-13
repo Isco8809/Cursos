204fa3f23a04c3e55bf9a13f8c8d07829cd3cc2d
Isco8809 <francisco.echeverri@bambooanalytics.com.co>
2021-09-12 21:56:38 -0500

    Creados los metodos de conexion a las monedas y al json

--- Python/ClaseProyectoFinalPython.py
+++ Python/ClaseProyectoFinalPython.py
@@ -1,23 +1,49 @@
-import json
+import json, requests
 #Se crea la clase que se encarga de consultar los datos del usuario (codigo, criptmonedas y cantidad)
 class usuario():
     def __init__(self, codigo):
         self.codigo = codigo
 
+#En este metodo creamos la conexión al json, que tiene la informació de los usuarios, se lee los datos
     def consultarBD(self):
         with open('Python\BD_Usuarios.json') as js:
             consulta = json.load(js)
             js.close()
         return consulta
 
+#Verificación de que el usuario ingresado este en el json de los datos para el login
     def codigoUsuario(self):
-        conexion = self.consultarBD()
+        verificacion=True
+        Datos = self.consultarBD()
+        while verificacion:
+            for a in Datos['Usuarios']:
+                if self.codigo in str(a['Codigo']):
+                    x=a
+                    verificacion=False
+            if  verificacion:
+                self.codigo = input("El codigo no se encuentra, por favor ingrese el codigo correcto: ")
+        return x
+
+#Conexión a la página de criptomonedas
+    def conexionCriptomonedas(self):
+        headers = {  'Accepts': 'application/json',  
+                    'X-CMC_PRO_API_KEY':  'b1bc1067-1001-4842-a6c8-f82d1dba7d25'}
+        conexion = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",headers=headers).json()
+        return conexion
+
+#Creamos una lista de las criptomonedas de la página, para una rápida validación de que existan
+    def listaCriptomonedas(self):
+        datos = self.conexionCriptomonedas()
+        lista=[]
+        for campos in datos['data']:
+            lista.append(campos['symbol'])
+        tuple(lista)
+        return lista
         
-        if self.codigo == conexion['Usuarios']['Criptomonedas']:
-            
-        for a in otra['Usuarios'][0]['Criptomonedas'][0]['Cripto']:
-            print(a)
-        return a
-    
-    # def datosUsuario(self):
-        
+#Lista de criptomonedas que tiene el usuario
+    def criptomonedaUsuario(self):
+        datos_usuario = self.codigoUsuario()
+        lista = []
+        for valor in datos_usuario['Criptomonedas']['Cripto']:
+            lista.append(valor)
+        return lista
--- Python/mainPrincipal.py
+++ Python/mainPrincipal.py
@@ -1,14 +1,7 @@
 import json
-#from ClaseProyectoFinalPython import usuario
+from ClaseProyectoFinalPython import usuario
 
-with open('Python\BD_Usuarios.json') as js:
-    consulta = json.load(js)
-    js.close()
-    print(consulta)
-    
-name = input()
-
-for a in consulta['Usuarios']:
-    if name in str(a['Codigo']):
-        print(a['Nombre'])
-        pass
+x = input()
+metodo = usuario(x)
+respuesta = metodo.criptomonedaUsuario()
+print(respuesta)
